0817abc95188456e49a76045bc02180225d07ad6
Kelly N <kellynarboux@hotmail.fr>
2018-12-07 07:25:18 +0100

    Finish Alien game

--- Assets/Scripts/AlienGame/GameManager.cs
+++ Assets/Scripts/AlienGame/GameManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-    enum GamePhase {afficheAlienRandom, afficheValeurs, afficheResultat, afficheScore}
+    enum GamePhase {afficheEnonce, afficheAlienRandom, afficheValeurs, afficheResultat}
 
     public GameObject prefabAlien;
+    public Text prefabValeur;
     public int NB_COLORS;
     public int NB_ALIENS;
     private GameObject[] aliens;
@@ -17,12 +18,24 @@ public class GameManager : MonoBehaviour {
     private bool valeurAffichees = false;
     private GamePhase phase;
     private Text[] textValeurCouleurs;
+    public GameObject myText;
+    private Text text;
+    public Canvas canvas;
+    private int result;
+    public GameObject inputField;
+
+    int resultTrue;
+    bool test;
 
     // Use this for initialization
     void Start () {
-        phase = GamePhase.afficheAlienRandom;
+        text = myText.GetComponent<Text>();
+        phase = GamePhase.afficheEnonce;
         initialiseCouleurs();
         textValeurCouleurs = new Text[NB_COLORS];
+        timer = 5;
+        resultTrue = 0;
+        test = false;
     }
 
 	// Update is called once per frame
@@ -32,6 +45,13 @@ public class GameManager : MonoBehaviour {
        
         switch (phase)
         {
+            case GamePhase.afficheEnonce:
+                if (timer < 0)
+                {
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, 0f);
+                    phase = GamePhase.afficheAlienRandom;         
+                }
+                    break;
             case GamePhase.afficheAlienRandom:
                 if (!aliensVisibles)
                 {
@@ -47,22 +67,38 @@ public class GameManager : MonoBehaviour {
             case GamePhase.afficheValeurs:
                 if (!valeurAffichees)
                 {
+                    inputField.SetActive(true);
                     creationAliensParCouleurEtAffichageTextes();
-                    
                 }
                 break;
 
             case GamePhase.afficheResultat:
-              
-                break;
-
-            case GamePhase.afficheScore:
-               
+                string textR;
+                if (result == resultTrue) textR = "Bravo !";
+                else textR = "Dommage ! Tu peux faire mieux";
+                text.text = result + " / " + resultTrue + "\n" + textR;
+                text.color = new Color(text.color.r, text.color.g, text.color.b, 1f);
                 break;
 
             default:
                 break;
 
+        }
+    }
+
+    public void getEntrer(string entre)
+    {
+        Debug.Log(inputField.GetComponent<InputField>().text);
+        result = int.Parse(inputField.GetComponent<InputField>().text);
+        Debug.Log("wow");
+        phase = GamePhase.afficheResultat;
+        inputField.SetActive(false);
+        desactiveAliens();
+
+        var clones = GameObject.FindGameObjectsWithTag("Valeur");
+        foreach (var clone in clones)
+        {
+            Destroy(clone);
         }
     }
 
@@ -102,7 +138,7 @@ public class GameManager : MonoBehaviour {
     {
         initialiseAliens();
         aliensVisibles = true;
-        timer = 2;
+        timer = 3;
     }
 
     void desactiveAliens()
@@ -133,30 +169,48 @@ public class GameManager : MonoBehaviour {
         //view infos : 
         Camera camera = Camera.main;
         float halfHeight = camera.orthographicSize;
-        float halfWidth = camera.aspect * halfHeight;
+        float halfWidth = camera.aspect * halfHeight - 6;
 
         float x = -halfWidth + 1;
         float y = halfHeight - 3;
         int i = 0;
-        while(i < NB_COLORS)
+    
+        float xv = -9;
+        float yv = -3;
+
+        while (i < NB_COLORS)
         {
             Vector2 position = new Vector2(x,y);
             aliens[i] = creerAlien(colors[i], position);
             i++;
-            if ((x+3) < halfWidth)
+            if (i == 6)
             {
-                x+=3;
+                xv = 0;
+                yv -= 9;
             }
             else
+            {
+                xv +=9;
+            }
+            if (i == 5)
             {
                 x = -halfWidth + 1;
                 y -= 3;
             }
+            else
+            { 
+                x += 3;
+            }
+
+            Color colorAlien = aliens[i-1].GetComponent<Alien>().GetColor();
+            Text valeur = Instantiate(prefabValeur, new Vector2(0,0), Quaternion.identity);
+            valeur.transform.SetParent(canvas.transform, false);
+            resultTrue += valeurCouleurs[colorAlien];
+            valeur.text = valeurCouleurs[colorAlien].ToString();
+            RectTransform assignText = valeur.GetComponent<RectTransform>();
+            assignText.transform.position = new Vector2(assignText.transform.position.x + xv*10, assignText.transform.position.y + yv*10);
             
         }
-
-        
-
 
         valeurAffichees = true;
     }
