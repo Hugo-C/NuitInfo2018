06c1f30d7c1416fe5c64eabb2bddb7086e513496
Kelly N <kellynarboux@hotmail.fr>
2018-12-07 03:17:38 +0100

    Add alarm game

---
+++ Assets/Scripts/AlarmGame/Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite sprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    public GameObject levelManager;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown()
+    {
+        if (levelManager.GetComponent<LevelManager>().counterActive)
+        {
+            spriteRenderer.sprite = sprite;
+            levelManager.GetComponent<LevelManager>().StopCounter();
+        }
+    }
+}
---
+++ Assets/Scripts/AlarmGame/MyText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyText : MonoBehaviour {
+
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        var wantedPos = Camera.main.WorldToScreenPoint(target.transform.position);
+        transform.position = wantedPos;
+
+    }
+}
